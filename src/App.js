5b56ebc7acc0977eb513b6731e58856f12cec9f6
Ashok K <kotakommulaashok22@gmail.com>
2020-11-27 22:02:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import TodoList from "./list";
+
+function App() {
+  return (
+    <div id="main">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/editTodo.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import TodoForm from "./form";
+
+const Todo = (props) => {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: ""
+  });
+
+  const submitUpdate = (value) => {
+    props.updateTodo(edit.id, value);
+    setEdit({
+      id: null,
+      value: ""
+    });
+  };
+
+  if (edit.id) {
+    return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+  }
+
+  return props.todos.map((todo, index) => (
+    <div className="box" key={index}>
+      <div
+        key={todo.id}
+        className="list"
+        className={todo.isComplete ? "list complete" : "list"}
+        onClick={() => props.completeTodo(todo.id)}
+      >
+        {todo.text}
+      </div>
+      <div>
+        <button
+          className="align-one"
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+        >
+          Edit
+        </button>
+        <button className="align-one" onClick={() => props.removeTodo(todo.id)}>
+          Delete
+        </button>
+      </div>
+      <div></div>
+    </div>
+  ));
+};
+
+export default Todo;
